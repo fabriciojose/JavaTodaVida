d2c1c5b6665ac693ff9540e3ce4bba1f43636d19
fabricio.jose <fabmus1@gmail.com>
2018-01-03 22:27:17 -0200

    Exemplo do livro funcionando.

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,67 +15,89 @@ import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
 	
-	// criar os controladores como variáveis globais
-		TextField txtLogin;
-		PasswordField txtSenha;
+	// criar os componentes como variáveis globais
+	
 		
-		Button btnEntrar = new Button ("Entrar");
-		Button btnSair = new Button ("Sair");
+		private AnchorPane pane;
+		private TextField txtLogin;
+		private PasswordField txtSenha;
+		private Button btnEntrar, btnSair;
+		private static Stage stage;
 		
-		// método principal
-		public static void main(String[] args) {
-			
-
-		}
-
-		// método start
+		
+		
+		@Override // método start
 		public void start(Stage stage) throws Exception {
 			
-			// painel de ancoragem
-			AnchorPane pane = new AnchorPane();
-			pane.setPrefSize(400, 500);
-			
-			txtLogin.setText("Digite seu id");
-			txtLogin.setText("Digite a senha");
-					
-			pane.getChildren().add(txtLogin);
-			pane.getChildren().add(txtSenha);
-			pane.getChildren().add(btnEntrar);
-			pane.getChildren().add(btnSair);
+			initComponents();
+			initListeners();
 			
 			//criar uma cena
 			Scene scene = new Scene(pane);
-			// com a cena, o container principal da aplicação, dela se define o layout dos componentes
 			
+			// com a cena, o container principal da aplicação, dela se define o layout dos componentes
 			stage.setScene(scene);
 			
-			// implementar o CSS
-			pane.setStyle("fx-background-color: linear-gradient (from 0% to 100% 100% blue 0% silver 100%)");
+			stage.setResizable(false);
 			
+			stage.setTitle("Login - Adasa");
 			
 			stage.show();
+				
+			initLayout();
+			
+			//indicar a variável stage
+			Main.stage = stage;
+			
+			// chamar o método
+			
+		}
+		
+		// criar o geter da variável stage
+		public static Stage getStage () {
+			return stage;
+		}
+		
+		private void initComponents() {
+			
+			pane = new AnchorPane();
+			pane.setPrefSize(300, 350);
+			
+			txtLogin = new TextField();
+			txtLogin.setPromptText("Digite seu id");
+			
+			txtSenha = new PasswordField();
+			txtSenha.setPromptText("Digite a senha:");
+			
+			btnEntrar  = new Button("Entrar");
+			
+			btnSair = new Button ("Sair");
+			
+			pane.getChildren().addAll(txtLogin, txtSenha, btnEntrar, btnSair);
+			
+			
+			
+			
+		}
+		private void initLayout () {
 			
 			// definir o layout dos componentes
 			txtLogin.setLayoutX((pane.getWidth() - txtLogin.getWidth())/2);
 			txtLogin.setLayoutY(50);
-			
+						
 			txtSenha.setLayoutX((pane.getWidth() - txtSenha.getWidth())/2);
 			txtSenha.setLayoutY(100);
-			
+						
 			btnEntrar.setLayoutX((pane.getWidth() - btnEntrar.getWidth())/2);
-			btnEntrar.setLayoutY(100);
-		
+			btnEntrar.setLayoutY(150);
+					
 			btnSair.setLayoutX((pane.getWidth() - btnSair.getWidth())/2);
 			btnSair.setLayoutY(200);
-			
-			// chamar o método
-			initListeners();
-			
-			
-			
+						
+			// implementar o CSS
+			pane.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, blue 0%, silver 100%);");
 		}
-		
-		
+				
 		private void initListeners () {
 			// criar ação para o botão sair
 			btnSair.setOnAction(new EventHandler <ActionEvent>() {
@@ -91,8 +113,10 @@ public class Main extends Application {
 					logar();
 				}
 			});
-							
+				
+			
 		}
+		
 		private void logar () {
 			if (txtLogin.getText().equals("admin") && txtSenha.getText().equals("123")) {
 				JOptionPane.showMessageDialog(null, "Você entrou com sucesso!");
@@ -102,5 +126,10 @@ public class Main extends Application {
 		}
 		private void fecharAplicacao () {
 			System.exit(0);
+		}
+		
+		// método principal
+		public static void main(String[] args) {
+			launch (args);
 		}
 }
